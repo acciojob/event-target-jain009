5a230164a9c2a426343cbd438c0510af5a466cd0
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-04-17 21:33:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,29 @@ class EventTarget {
     }
   
     addEventListener(event, callback) {
+		if(!this._listeners[event]){
+			this._listeners[event]=[];
+		}
+		if(!this._listners[event].includes(callback)){
+			this._listners[event].push(callback);
+		}
       
     }
   
     removeEventListener(event, callback) {
-      
+      if(this._listeners[event]){
+		  this._listeners[event] = this._listners[event].filter(
+			  (listner) => listner !== callback;
+		  )
+	  }
     }
   
     dispatchEvent(event) {
-    
+    if(this._listeners[event]){
+		this._listeners[event].forEach((listener) => {
+			listener();
+		})
+	}
     }
-    
   }
   
